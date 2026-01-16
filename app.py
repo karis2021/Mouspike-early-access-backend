899dddc233b61d74f2d45c4d112296d59910ce83
karis232 <u2910841@uel.ac.uk>
2026-01-16 18:30:10 +0000

    Update .py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from email_service import send_thank_you_email
 from database import init_db, insert_email, list_signups
 
 app = FastAPI(title="Mouspike Early Access API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "https://karis2021.github.io"],
-    allow_credentials=False,
-    allow_methods=["POST", "GET", "OPTIONS"],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
@@ -21,6 +21,7 @@ def on_startup():
 @app.get("/health")
 def health_check():
     return{"status":"ok"}
+
 class SignupRequest(BaseModel):
     email: str
 
@@ -30,14 +31,16 @@ def signup(payload: SignupRequest):
     result = insert_email(email)
 
     if result["inserted"]:
+        demo_receiver = os.getenv("TEST_RECEIVER_EMAIL", email)
         try:
-            send_thank_you_email(email)
+            send_thank_you_email(demo_receiver)
         except Exception as e:
             print("Welcome email failed:", repr(e))
         return {
             "status": "ok",
             "message": "Email Registered Successfully",
-            "email": email
+            "email": email,
+           "email_sent_to": demo_receiver
         }
     return {
         "status": "Exists",
@@ -46,7 +49,5 @@ def signup(payload: SignupRequest):
     }
 @app.get("/signups")
 def get_signups(limit: int = 100):
-    return {
-        "count": len(list_signups(limit)),
-        "items": list_signups(limit)
-    }
+    items = list_signups(limit)
+    return {"count": len(items), "items": items}
